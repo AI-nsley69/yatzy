6a8582b9ec375de19c204a395b2f42db9602ca99
20bear <20bear@stockholmscience.se>
2023-03-26 19:49:57 +0200

    Try the deploy workflow

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="navbar-thingy">
-    <h1 class="navbar-text">Yatzy</h1>
+      <h1 class="navbar-text">Yatzy</h1>
   </div>
-  <h1 class="text-center text-secondary-content">Hello World</h1>
+  
 </template>
