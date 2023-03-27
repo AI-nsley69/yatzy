1459aa8aeccc8eb0473a1e864eb87c7c63566156
20bear <20bear@stockholmscience.se>
2023-03-27 12:37:44 +0200

    non-working info bar

--- src/App.vue
+++ src/App.vue
@@ -21,21 +21,28 @@
     ];
     return diceNames[index - 1] || 'fa-dice-one';
   };
+
+  const onDelete = () => {
+    totalScore.value = 15;
+    currentRoll.forEach((die) => {
+      die.value = 1;
+    });
+  };
 </script>
 
 <template>
   <div class="navbar-thingy">
       <h1 class="navbar-text">Yatzy</h1>
   </div>
-  <div class="flex flex-col px-6 py-6">
+  <div class="flex flex-col px-8 py-8 justify-center w-min">
     <div class="flex flex-col justify-center items-center bg-secondary lg:w-64 lg:h-64 md:w-40 md:h-40 rounded-xl p-1">
       <div class="text-2xl flex flex-row gap-1">
         <h3>Total score: </h3>
         <h3>{{ totalScore }}</h3>
       </div>
-      <div class="flex flex-row justify-evenly gap-1 dice-row">
+      <div class="flex flex-row justify-evenly gap-2 dice-row">
         <template v-for="(die, index) in currentRoll">
-          <i :class="['fas', getDiceNames(die.value)]" class="text-5xl" ></i>
+          <i :class="['fas', getDiceNames(die.value)]" class="text-3xl" ></i>
         </template>
       </div>
       <button 
@@ -45,6 +52,12 @@
       >
         Roll
       </button>
+    </div>
+
+    <div class="info-row flex flex-row justify-evenly items-center bg-info w-32 h-10 rounded-xl self-center my-4">
+      <button><i class="fa-solid fa-question"></i></button>
+      <button><i class="fa-solid fa-clock-rotate-left"></i></button>
+      <button><i class="fa-solid fa-trash"></i></button>
     </div>
   </div>
 </template>
