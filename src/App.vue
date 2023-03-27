7140cf1b2ac58ddf963797d828eb842db6957754
20bear <20bear@stockholmscience.se>
2023-03-27 10:41:48 +0200

    non-working dice roll

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,48 @@
 <script setup>
   import { ref } from 'vue';
-  const totalScore = ref(3);
+  const currentRoll = [ref(1), ref(2), ref(3), ref(4), ref(5)];
+  let totalScore = ref(15);
+
+  const rollDice = () => {
+    totalScore = 0;
+    currentRoll.forEach((die) => {
+      die.value = Math.floor(Math.random() * 6) + 1;
+      totalScore += die.value;
+    });
+
+    document.getElementsByName('i').forEach((tag, index) => {
+      tag.classList.remove('fa-dice-one', 'fa-dice-two', 'fa-dice-three', 'fa-dice-four', 'fa-dice-five', 'fa-dice-six');
+      console.log(getDiceNames(index));
+      tag.classList.add(getDiceNames(index));
+    })
+  };
+
+  const getDiceNames = (idx) => {
+    switch (currentRoll[idx].value) {
+      case 1:
+        return 'fa-dice-one';
+      case 2:
+        return 'fa-dice-two';
+      case 3:
+        return 'fa-dice-three';
+      case 4:
+        return 'fa-dice-four';
+      case 5:
+        return 'fa-dice-five';
+      case 6:
+        return 'fa-dice-six';
+      default:
+        return 'fa-dice-one';
+    }
+  };
 </script>
 
 <template>
   <div class="navbar-thingy">
       <h1 class="navbar-text">Yatzy</h1>
   </div>
-  <div>
-    <div class="fixed flex flex-col justify-center items-center bg-secondary top-24 left-24 w-64 h-64 rounded-xl">
+  <div class="flex flex-col px-6 py-6">
+    <div class="flex flex-col justify-center items-center bg-secondary w-64 h-64 rounded-xl">
       <div class="text-2xl flex flex-row gap-1">
         <h3>Total score: </h3>
         <h3>{{ totalScore }}</h3>
@@ -20,7 +54,13 @@
         <i class="fa-solid fa-dice-four"></i>
         <i class="fa-solid fa-dice-five"></i>
       </div>
-      <button class="bg-primary text-white rounded-xl w-16 h-10 mt-4">Roll</button>
+      <button 
+        type="input"
+        @click="rollDice()"
+        class="bg-primary text-white rounded-xl w-16 h-10 mt-4 hover:bg-primary-focus active:resize"
+      >
+        Roll
+      </button>
     </div>
   </div>
 </template>
