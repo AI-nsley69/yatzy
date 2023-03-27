c783fc94eb0e69ac146935a50ec2f9a07edb38be
20bear <20bear@stockholmscience.se>
2023-03-27 11:01:40 +0200

    Working dice roll

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,26 @@
 <script setup>
   import { ref } from 'vue';
   const currentRoll = [ref(1), ref(2), ref(3), ref(4), ref(5)];
-  let totalScore = ref(15);
+  const totalScore = ref(15);
 
   const rollDice = () => {
-    totalScore = 0;
+    console.log('Rolling dice');
     currentRoll.forEach((die) => {
       die.value = Math.floor(Math.random() * 6) + 1;
-      totalScore += die.value;
     });
-
-    document.getElementsByName('i').forEach((tag, index) => {
-      tag.classList.remove('fa-dice-one', 'fa-dice-two', 'fa-dice-three', 'fa-dice-four', 'fa-dice-five', 'fa-dice-six');
-      console.log(getDiceNames(index));
-      tag.classList.add(getDiceNames(index));
-    })
+    totalScore.value = currentRoll.reduce((acc, die) => acc + die.value, 0);
   };
 
-  const getDiceNames = (idx) => {
-    switch (currentRoll[idx].value) {
-      case 1:
-        return 'fa-dice-one';
-      case 2:
-        return 'fa-dice-two';
-      case 3:
-        return 'fa-dice-three';
-      case 4:
-        return 'fa-dice-four';
-      case 5:
-        return 'fa-dice-five';
-      case 6:
-        return 'fa-dice-six';
-      default:
-        return 'fa-dice-one';
-    }
+  const getDiceNames = (index) => {
+    const diceNames = [
+      'fa-dice-one',
+      'fa-dice-two',
+      'fa-dice-three',
+      'fa-dice-four',
+      'fa-dice-five',
+      'fa-dice-six',
+    ];
+    return diceNames[index - 1] || 'fa-dice-one';
   };
 </script>
 
@@ -42,22 +29,20 @@
       <h1 class="navbar-text">Yatzy</h1>
   </div>
   <div class="flex flex-col px-6 py-6">
-    <div class="flex flex-col justify-center items-center bg-secondary w-64 h-64 rounded-xl">
+    <div class="flex flex-col justify-center items-center bg-secondary lg:w-64 lg:h-64 md:w-40 md:h-40 rounded-xl p-1">
       <div class="text-2xl flex flex-row gap-1">
         <h3>Total score: </h3>
         <h3>{{ totalScore }}</h3>
       </div>
       <div class="flex flex-row justify-evenly gap-1 dice-row">
-        <i class="fa-solid fa-dice-one"></i>
-        <i class="fa-solid fa-dice-two"></i>
-        <i class="fa-solid fa-dice-three"></i>
-        <i class="fa-solid fa-dice-four"></i>
-        <i class="fa-solid fa-dice-five"></i>
+        <template v-for="(die, index) in currentRoll">
+          <i :class="['fas', getDiceNames(die.value)]" class="text-5xl" ></i>
+        </template>
       </div>
       <button 
         type="input"
         @click="rollDice()"
-        class="bg-primary text-white rounded-xl w-16 h-10 mt-4 hover:bg-primary-focus active:resize"
+        class="bg-primary text-white rounded-xl w-16 h-10 mt-4 hover:bg-primary-focus active:simple-shadow"
       >
         Roll
       </button>
