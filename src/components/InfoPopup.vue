5a88c3ba3a129bbfc10ee0032c552a278619f94c
20bear <20bear@stockholmscience.se>
2023-04-05 11:35:29 +0200

    Improve a tag colors on the info popup

--- src/components/InfoPopup.vue
+++ src/components/InfoPopup.vue
@@ -5,7 +5,7 @@
 <template>
     <div class="wrapper bg-info text-sm w-4/5 md:w-3/5 md:text-lg blur-none shadow-lg p-4 justify-center m-3">
         <h1 class="font-bold">Information</h1>
-        <ul class="list-disc text-neutral-content">
+        <ul class="list-disc text-neutral-content w-fit">
             <li>This is a simple yatzy site, you can roll 5 dices at once and see your 10 latest rolls.</li>
             <li>This site was built through Vue and TailwindCSS using Vite as the building tooling.</li>
             <li>If you wish to check out the source, you can find it <a href="https://github.com/AI-nsley69/yatzy" class="link">here!</a></li>
@@ -31,7 +31,7 @@
 }
 
 .link {
-    @apply underline text-accent hover:scale-[.98] active:scale-[1.02];
+    @apply underline text-pink-300 hover:scale-[.98] active:scale-[1.02];
 }
 
 .list-disc > li {
