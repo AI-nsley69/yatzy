90f99e98994cf4192abbd489c49682aa953fbe03
20bear <20bear@stockholmscience.se>
2023-04-18 10:10:43 +0200

    Fix incorrect color for history & info

--- src/components/DeleteConfirmation.vue
+++ src/components/DeleteConfirmation.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-<div class="wrapper w-4/5 lg:w-2/5 text-center">
+<div class="wrapper-delete-confirmation w-4/5 lg:w-2/5 text-center">
     <h3>Are you sure you wish to fully reset the game?</h3>
     <h5 class="text-error mb-4">This will reset your entire history, current rolls and rounds played!</h5>
     <div class="flex flex-row justify-evenly w-full">
@@ -26,7 +26,7 @@
 </template>
 
 <style>
-.wrapper {
+.wrapper-delete-confirmation {
     @apply fixed flex flex-col justify-start items-center h-fit bg-neutral-focus rounded-lg p-4 m-2 border-neutral-800 border-4 gap-1;
     top: 50%;
     left: 50%;
